948641c825b7fbcf26ebca97dca9547bff6fd463
kimijammy <125917969+kimijammy@users.noreply.github.com>
2023-03-21 21:46:58 +0000

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,51 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Person {
+    private String name;
+    private int age;
+    double height;
+    double weight;
+
+    public Person(String name, int age, double height, double weight){
+        setName(name);
+        setAge(age);
+        setHeight(height);
+        setWeight(weight);
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void growOlder(){
+        age++;
+        height--;
+    }
+
 }
